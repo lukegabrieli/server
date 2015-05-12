f299aa88fec7f7091d014dc9fa907f22a81df32c
Luke Gabrieli <usetheforce10@gmail.com>
2015-05-12 00:18:31 -0700

    add most updated server.js file

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
 var http = require("http");
 var url = require("url");
+var path = require("path");
 var fs = require("fs");
 var port = process.argv[2] || 3000;
 
-
 var server = http.createServer(function(req, res) {
-  var url = url.parse(req.url).path;
+  var index = "";
+  var folder = __dirname+"/";
+  var file = path.basename(req.url);
 
-  fs.readFile("index.html", function(err, page) {
-    if(err) {
-      res.writeHead(500, {"content-type": "text/plain"});
-      res.write("Error!");
-      res.end();
-    };
+  if(file === "index.html") {
+    index = folder + file;
 
-    res.writeHead(200, {"content-type": "text/plain"});
-    res.end(page);
-  });
+    fs.readFile(index, function(err, page) {
+      if(err) {
+        res.writeHead(500, {"content-type": "text/plain"});
+        res.write("Error!");
+        res.end();
+      };
+
+      res.writeHead(200, {"content-type": "text/html"});
+      res.end(page);
+    });
+  }
+  else {
+    res.writeHead(404, {"content-type": "text/plain"});
+    res.end("404 Error: This page cant be found.");
+  };
 });
 
-server.listen(Number(process.argv[2]));
-console.log("Server running at Port "+port+"\nPress CONTROL + C to quit");
+server.listen(3000);
+console.log("Server running at http://localhost:"+port+"index.html\nPress CONTROL + C to quit");
